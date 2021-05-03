74ece1bad3ecb552bc7ae5fb04c8a14a8c4a33af
RiyadTangil <riyadkhanxp2@gmail.com>
2021-05-03 13:42:08 +0600

    done

--- forms/contact.php
+++ forms/contact.php
@@ -30,4 +30,21 @@
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
+
+
+
+if($_POST["submit"]) {
+    $recipient="your@email.address";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
 ?>
